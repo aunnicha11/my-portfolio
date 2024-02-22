6efebb8c26dd1223910092a9faacd9532f17cdc3
Louktarn <aunnicha268@gmail.com>
2024-02-22 18:17:32 +0700

    fix link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -124,7 +124,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://drive.google.com/drive/folders/1k5eQldn-I2Nwv7ECxOaZ6yHMEjyX-1vo?usp=sharing'
               target='_blank'
             >
               Resume & Docs <BsFillPersonLinesFill size={30} />
